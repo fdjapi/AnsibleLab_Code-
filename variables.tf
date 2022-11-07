162228b56473524cb632b33d9caa474f8e67721b
cloudtech-services <djapif@gmail.com>
2022-11-07 14:01:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_ami_id" {
 }
 
 variable "ssh_key_pair" {
-  default = file("LINUX.pem")
+  default ="LINUX.pem"
   #default = "~/.ssh/id_rsa_ansilble_lab"
 }
 
 variable "ssh_key_pair_pub" {
-  default = file("LINUX.pub")
+  default = "LINUX.pub"
   #default = "~/.ssh/id_rsa_ansilble_lab.pub"
 }
 
